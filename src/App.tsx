2b3bf6a498cb3020cd1d662b1af8008e4e0c11ae
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-02 20:32:12 -0300

    Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components"
 import { Button } from "./components/Button"
+import { GlobalStyle } from "./styles/global"
 
 import { defaultTheme } from "./styles/themes/default"
 
@@ -11,6 +12,8 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
