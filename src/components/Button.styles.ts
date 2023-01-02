b2a1e16a5e93118a4f6a91c0ac75b554c6ad3633
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-02 18:50:34 -0300

    Configurando temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) => {
+  background-color: ${(props) => props.theme.primary};
+
+  /* ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `
-  }}
+  }} */
 `
